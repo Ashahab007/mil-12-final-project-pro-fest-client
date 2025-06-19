4dbe132a88b7428b2a4ec4162e3a86641e62b2f9
Ashahab007 <ashahab007@gmail.com>
2025-06-20 02:13:57 +0600

    initial setup react router,tailwind and daisyui with navbar, home and footer completed

---
+++ src/shared/ProFast/ProFast.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import logo from "../../assets/logo.png";
+
+const ProFast = () => {
+  return (
+    <div>
+      <img src={logo} alt="" srcset="" />
+      Profast
+    </div>
+  );
+};
+
+export default ProFast;
