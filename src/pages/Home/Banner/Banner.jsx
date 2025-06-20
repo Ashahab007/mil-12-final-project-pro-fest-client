a7fe4d0bdc5cb638fbb86a767fba1dfb6cd448a7
Ashahab007 <ashahab007@gmail.com>
2025-06-20 15:09:06 +0600

    updated services section

--- src/pages/Home/Banner/Banner.jsx
+++ src/pages/Home/Banner/Banner.jsx
@@ -1,8 +1,8 @@
 import React, { Component } from "react";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
-import banner1 from "../../../../assets/banner/banner1.png";
-import banner2 from "../../../../assets/banner/banner2.png";
-import banner3 from "../../../../assets/banner/banner3.png";
+import banner1 from "../../../assets/banner/banner1.png";
+import banner2 from "../../../assets/banner/banner2.png";
+import banner3 from "../../../assets/banner/banner3.png";
 import { Carousel } from "react-responsive-carousel";
 
 const Banner = () => {
