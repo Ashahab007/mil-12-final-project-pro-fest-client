0e5fc06e796307038e2dfe54e0346cc1a1dc7feb
Ashahab007 <ashahab007@gmail.com>
2025-06-20 14:50:45 +0600

    created services section

--- src/pages/Home/Home/Banner/Banner.jsx
+++ src/pages/Home/Home/Banner/Banner.jsx
@@ -1,13 +1,18 @@
 import React, { Component } from "react";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
-import banner1 from "../../assets/banner/banner1.png";
-import banner2 from "../../assets/banner/banner2.png";
-import banner3 from "../../assets/banner/banner2.png";
+import banner1 from "../../../../assets/banner/banner1.png";
+import banner2 from "../../../../assets/banner/banner2.png";
+import banner3 from "../../../../assets/banner/banner3.png";
 import { Carousel } from "react-responsive-carousel";
 
 const Banner = () => {
   return (
-    <Carousel autoPlay={true} infiniteLoop={true} transitionTime={2000}>
+    <Carousel
+      autoPlay={true}
+      infiniteLoop={true}
+      transitionTime={2000}
+      showThumbs={false}
+    >
       <div>
         <img src={banner1} />
         <p className="legend">Legend 1</p>
