259be2f94163031f7eb453fe4ec2287616a2df51
Ashahab007 <ashahab007@gmail.com>
2025-06-21 20:14:37 +0600

    implemented error message if validation fails

---
+++ src/pages/Authentication/Register/Register.jsx
@@ -0,0 +1,65 @@
+import React from "react";
+import { useForm } from "react-hook-form";
+import { data } from "react-router";
+
+// 5.0 My requirement is applying react hook form in Register section
+// 6.0 My requirement is show error in the form if validation is failed
+const Register = () => {
+  // 5.3 call the onSubmit as per doc
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+  // 5.1 destructure register, handleSubmit from useForm as per doc
+  const {
+    register,
+    handleSubmit,
+    // 6.1 destructure formState: { errors } from useForm
+    formState: { errors },
+  } = useForm();
+
+  return (
+    <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+      <h3>Create Your Account</h3>
+      <form onSubmit={handleSubmit(onSubmit)} className="card-body">
+        <fieldset className="fieldset">
+          <label className="label">Email</label>
+          <input
+            type="email"
+            // 5.2.0 apply the register to get the email data
+            // 6.2 apply the validation for required
+            {...register("email", { required: true })}
+            className="input"
+            placeholder="Email"
+          />
+          {/* 6.3 show error if validation fails*/}
+          {errors.email?.type === "required" && (
+            <p className="text-red-500">Email is required</p>
+          )}
+          <label className="label">Password</label>
+          <input
+            type="password"
+            // 5.2.1 apply the password to get the password data
+            // 6.5 apply length for password validation
+            {...register("password", { minLength: 6 })}
+            className="input"
+            placeholder="Password"
+          />
+          {/* 6.4 show error if validation is failed for password length */}
+          {errors.password?.type === "minLength" && (
+            <p className="text-red-500">
+              Password at least 6 characters or more
+            </p>
+          )}
+          <div>
+            <a className="link link-hover">Forgot password?</a>
+          </div>
+          <button type="submit" className="btn btn-neutral mt-4">
+            Register
+          </button>
+        </fieldset>
+      </form>
+    </div>
+  );
+};
+
+export default Register;
