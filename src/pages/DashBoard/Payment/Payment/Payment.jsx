8e9b7921f4346fe1900f8db5db5ef15e2491fa49
Ashahab007 <ashahab007@gmail.com>
2025-06-28 02:21:16 +0600

    implemented  payment method in client side

--- src/pages/DashBoard/Payment/Payment/Payment.jsx
+++ src/pages/DashBoard/Payment/Payment/Payment.jsx
@@ -1,18 +1,21 @@
 import { Elements } from "@stripe/react-stripe-js";
 import { loadStripe } from "@stripe/stripe-js";
 import React from "react";
+import PaymentForm from "../PaymentForm/PaymentForm";
 
 // 21.0 My requirement is implement payment method using stripe js. from "https://github.com/stripe/react-stripe-js" and run "npm install @stripe/react-stripe-js @stripe/stripe-js" in client side.
 
 // 21.1 create a Payment component
 
-// 21.7 as per doc copy the following promise
+// 21.7 as per doc copy the following promise here the given key is for testing purpose. but we will change it later
 const stripePromise = loadStripe("pk_test_6pRNASCoBOKtIshFeQd4XMUh");
 
 const Payment = () => {
   return (
-    // 21.8 import the Elements and send stripePromise as per doc. but there is <CheckoutForm /> in doc u can do it according to the doc. but we will do it in different way
-    <Elements stripe={stripePromise}></Elements>
+    // 21.8 import the Elements and send stripePromise as per doc. then created the <PaymentForm> and set in the middle
+    <Elements stripe={stripePromise}>
+      <PaymentForm></PaymentForm>
+    </Elements>
   );
 };
 
--- src/pages/DashBoard/Payment/PaymentForm/PaymentForm.jsx
+++ src/pages/DashBoard/Payment/PaymentForm/PaymentForm.jsx
@@ -1,5 +1,5 @@
 import { CardElement, useElements, useStripe } from "@stripe/react-stripe-js";
-import React from "react";
+import React, { useState } from "react";
 
 // 21.8 created a payment form component
 const PaymentForm = () => {
@@ -7,8 +7,11 @@ const PaymentForm = () => {
   const stripe = useStripe();
   const elements = useElements();
 
+  // 21.14 show error for invalid card
+  const [error, setError] = useState("");
+
   // 21.10 created handle submit
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
 
     // 21.12 as per doc following three condition is created
@@ -16,21 +19,46 @@ const PaymentForm = () => {
       return;
     }
     // 21.12.1
-    const card = document.getElement(CardElement);
+    const card = elements.getElement(CardElement);
     // 21.12.2
     if (!card) {
       return;
     }
+
+    // 21.13 now creating the card payment method
+    const { error, paymentMethod } = await stripe.createPaymentMethod({
+      type: "card",
+      card,
+    });
+    if (error) {
+      console.log("error", error);
+      //   21.14.1
+      setError(error.message);
+    } else {
+      console.log("Payment Method", paymentMethod);
+      //   21.14.2
+      setError("");
+    }
   };
+
   // 21.9 created the form and import CardElement from @stripe/react-stripe-js and implement button as per doc
   return (
-    <form onSubmit={handleSubmit}>
-      <CardElement>
-        {/* 20.11.1 disabled if stripe is not present */}
-        <button type="submit" disabled={!stripe}>
-          Pay
-        </button>
+    <form
+      onSubmit={handleSubmit}
+      className="space-y-4 bg-white rounded-xl p-6 shadow-md w-full max-w-md mx-auto"
+    >
+      <CardElement className="p-2 border rounded">
+        {/* 21.11.1 disabled if stripe is not present */}
       </CardElement>
+      <button
+        className="btn btn-primary w-full"
+        type="submit"
+        disabled={!stripe}
+      >
+        Pay
+      </button>
+      {/* 21.14.3  now type in the form to test 4242 4242 4242 4242 and rest of the data is as ur wish*/}
+      {error && <p className="text-red-500">{error}</p>}
     </form>
   );
 };
